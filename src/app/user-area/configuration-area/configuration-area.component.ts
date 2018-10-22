19581fce12ec37e7cd637ae7bf02109786718123
danillofidel <danillopkt@gmail.com>
2018-10-22 00:25:00 -0300

    config form

--- src/app/user-area/configuration-area/configuration-area.component.ts
+++ src/app/user-area/configuration-area/configuration-area.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-configuration-area',
   templateUrl: './configuration-area.component.html',
-  styleUrls: ['./configuration-area.component.css']
+  styleUrls: ['./configuration-area.component.less']
 })
 export class ConfigurationAreaComponent implements OnInit {
+  public email: string = 'danillofidel@gmail.com'
 
   constructor() { }
 
