f21b554ff8ccfb72eeecf033da098f813ae89131
danillo <danillopkt@gmail.com>
2018-10-24 16:27:13 -0300

    start

--- src/app/user-area/autentication.service.ts
+++ src/app/user-area/autentication.service.ts
@@ -1,15 +1,16 @@
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { LoginCheckService } from '../login-check.service';
 
 @Injectable()
 export class AutenticationService implements CanActivate{
-  constructor(private auth: LoginCheckService){
+  constructor(private auth: LoginCheckService, private route : Router){
 
   }
 
   canActivate(){
     if(!this.auth.isAuth){
+      this.route.navigate(['/login'])
       return false
     }
       return true
