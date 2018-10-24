3b105a86653182eb9f506c87a8e82c935dfecf97
danillo <danillopkt@gmail.com>
2018-10-24 11:11:17 -0300

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginCheckService } from '../login-check.service';
 import { LogStateService } from '../log-state.service';
+import { isEmpty } from 'lodash';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,7 @@ import { LogStateService } from '../log-state.service';
 export class LoginComponent implements OnInit {
 
   public userType: number = 2;
-  public user: User;
+  public user: User[];
   @ViewChild('msgInvalid') msgInvalid: ElementRef
   public form: FormGroup = new FormGroup({
     'user': new FormControl(null, [ Validators.required ]),
@@ -26,59 +27,59 @@ export class LoginComponent implements OnInit {
     private renderer: Renderer2,
     private loginservice: LoginCheckService,
     private data: LogStateService
-  ) { }
+    ) { }
 
-  ngOnInit() {
-    //this.data.atualState.subscribe(state => this.isLogged = state)
-  }
+    ngOnInit() {
 
-  enterUserArea(): void{
-
-    console.log('formulario de login enviado', this.form)
-
-    if(this.validateInputFields()){
-      this.addInvalidFormMsg()
-    }else{
-      this.removeInvaliFormMsg()
     }
 
-    let userForm = this.form.value.user;
-    let passForm = this.form.value.password;
+    enterUserArea(): void{
+      if(this.validateInputFields()){
+        this.addInvalidFormMsg()
+      }else{
+        this.removeInvaliFormMsg()
+        let userForm = this.form.value.user;
+        let passForm = this.form.value.password;
 
-    this.loginservice.checkUser(userForm, passForm)
-    .then((data: any) => {
-      this.user = data;
-      console.log(this.user)
-    })
-    .catch((data: any) => {});
+        this.loginservice.checkUser(userForm, passForm)
+        .then((data: any) => {
+          this.user = data;
+          this.checkEmptyResponse(this.user);
+          if(this.validateUserLogin(this.user[0].email, this.user[0].pass)){
+            this.route.navigate(['/area-do-usuario'])
+            this.data.changeStateLogin(true)
+          }
+        })
+        .catch((data: any) => {});
+      }
 
-    if(this.validateUserLogin(this.user[0].email, this.user[0].pass)){
-      this.route.navigate(['/area-do-usuario'])
-      this.data.changeStateLogin(true)
-    }else{
-      console.log('erro');
+    }
+
+    validateUserLogin(name, password): boolean{
+      console.log("verficando correspondentes")
+      return name === this.form.value.user &&  password === this.form.value.password
+    }
+
+    validateInputFields(): boolean{
+      return this.form.status === 'INVALID'
+    }
+
+    addInvalidFormMsg(): void{
+      this.renderer.removeClass(this.msgInvalid.nativeElement, "panel__invalid-msg--hide");
+    }
+
+    removeInvaliFormMsg(): void{
+      this.renderer.addClass(this.msgInvalid.nativeElement, "panel__invalid-msg--hide");
+    }
+
+    checkEmptyResponse(response): void{
+      if(isEmpty(response)){
+        this.showRegisterMsg()
+      }
+    }
+
+    showRegisterMsg(): void{
+      alert('msg de registro!')
     }
 
   }
-
-  validateUserLogin(name, password): boolean{
-    return name === this.form.value.user &&  password === this.form.value.password
-  }
-
-  validateInputFields(): boolean{
-    return this.form.status === 'INVALID'
-  }
-
-  addInvalidFormMsg(): void{
-    alert('invalido')
-    this.renderer.removeClass(this.msgInvalid.nativeElement, "panel__invalid-msg--hide");
-  }
-
-  removeInvaliFormMsg(): void{
-    alert('valido')
-    this.renderer.addClass(this.msgInvalid.nativeElement, "panel__invalid-msg--hide");
-  }
-
-
-
-}
