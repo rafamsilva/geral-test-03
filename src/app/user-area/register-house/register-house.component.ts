92ea4f12f52bdaab1ed264a6d89eddb8686e9fe9
danillo <danillopkt@gmail.com>
2018-10-25 15:42:09 -0300

    update

--- src/app/user-area/register-house/register-house.component.ts
+++ src/app/user-area/register-house/register-house.component.ts
@@ -10,17 +10,17 @@ import { HousesService } from '../../houses.service';
 })
 export class RegisterHouseComponent implements OnInit {
   public form: FormGroup = new FormGroup({
-    'endereco': new FormControl(null, [Validators.required]),
-    'bairro': new FormControl(null, [Validators.required]),
-    'cidade': new FormControl(null, [Validators.required]),
+    'endereco': new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(130)]),
+    'bairro': new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]),
+    'cidade': new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(40)]),
     'estado': new FormControl(null, [Validators.required]),
-    'cep': new FormControl(null, [Validators.required]),
+    'cep': new FormControl(null, [Validators.required, Validators.pattern(/^[0-9]{8}$/)]),
     'tipo': new FormControl(null, [Validators.required]),
     'disp': new FormControl(null, [Validators.required]),
-    'suites': new FormControl(null, [Validators.required]),
-    'quartos': new FormControl(null, [Validators.required]),
-    'vagas': new FormControl(null, [Validators.required]),
-    'area': new FormControl(null, [Validators.required]),
+    'suites': new FormControl(null, []),
+    'quartos': new FormControl(null, []),
+    'vagas': new FormControl(null, []),
+    'area': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
     'valor': new FormControl(null, [Validators.required]),
   })
   public house: House;
@@ -28,13 +28,14 @@ export class RegisterHouseComponent implements OnInit {
   constructor(private houseService: HousesService) { }
 
   ngOnInit() {
-    console.log(this.form.value)
+    console.log('on init',this.form)
   }
 
   sendData(){
     this.house = this.form.value
-    this.houseService.registerHouse(this.house)
-    .subscribe((response)=> console.log(response))
+    console.log(this.form)
+    //this.houseService.registerHouse(this.house)
+    //.subscribe((response)=> console.log(response))
   }
 
 }
