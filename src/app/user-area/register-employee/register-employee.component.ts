c5131fe85974c102e2beece1442acdb0f689e009
danillo <danillopkt@gmail.com>
2018-10-26 10:16:55 -0300

    update

--- src/app/user-area/register-employee/register-employee.component.ts
+++ src/app/user-area/register-employee/register-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-employee',
@@ -8,16 +8,24 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class RegisterEmployeeComponent implements OnInit {
   public form: FormGroup = new FormGroup({
-    'name': new FormControl(null),
-    'lastName': new FormControl(null),
-    'phone': new FormControl(null),
-    'email': new FormControl(null),
-    'password': new FormControl(null)
+    'name': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+    'lastName': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(80)]),
+    'phone': new FormControl(null, [ Validators.required, Validators.pattern(/^[0-9]{11}$/)]),
+    'email': new FormControl(null, [ Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+    'password': new FormControl(null,  [ Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)]),
+    'password-confirm': new FormControl(null,  [ Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)])
   });
+
+
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.form)
+  }
+
+  sendData(): void{
+    console.log(this.form)
   }
 
 }
