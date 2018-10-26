11b82b12ba1202599d6c6a0e5f6f97415d9338b8
danillo <danillopkt@gmail.com>
2018-10-26 11:55:58 -0300

    update

--- src/app/user-area/register-employee/register-employee.component.ts
+++ src/app/user-area/register-employee/register-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Employee } from 'src/shared/employee.model';
 
 @Component({
   selector: 'app-register-employee',
@@ -7,25 +8,42 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register-employee.component.less']
 })
 export class RegisterEmployeeComponent implements OnInit {
+  public employee: Employee;
+  public isDifferent: boolean;
   public form: FormGroup = new FormGroup({
-    'name': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
-    'lastName': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(80)]),
-    'phone': new FormControl(null, [ Validators.required, Validators.pattern(/^[0-9]{11}$/)]),
+    'nome': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+    'sobrenome': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(80)]),
+    'telefone': new FormControl(null, [ Validators.required, Validators.pattern(/^[0-9]{11}$/)]),
     'email': new FormControl(null, [ Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
-    'password': new FormControl(null,  [ Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)]),
-    'password-confirm': new FormControl(null,  [ Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)])
+    'senha': new FormControl(null,  [ Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)]),
+    'confirmarSenha': new FormControl(null,  [ Validators.required])
   });
-
 
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.form)
+    console.log(this.form.value)
   }
 
-  sendData(): void{
-    console.log(this.form)
+  sendData(){
+    if(this.form.status !== 'INVALID' && this.confirmPassword()){
+      this.employee = this.form.value
+      alert('funcionario cadastrado!')
+      console.log(this.employee)
+      this.form.reset()
+    //this.houseService.registerHouse(this.house)
+    //.subscribe((response)=> console.log(response))
+    }
+  }
+
+  confirmPassword(){
+    if(this.form.value.senha != this.form.value.confirmarSenha){
+      alert('senhas não conferem')
+      this.isDifferent = true;
+      return false
+    }
+    return true
   }
 
 }
