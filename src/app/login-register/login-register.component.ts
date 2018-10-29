9da2d0f002094ca2a7fcf29be7c576ceae7c133d
danillofidel <danillopkt@gmail.com>
2018-10-29 18:51:00 -0300

    created

--- src/app/login-register/login-register.component.ts
+++ src/app/login-register/login-register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { matchPasswordValidator } from '../shared/password-match.validator';
 
 @Component({
   selector: 'app-login-register',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-register.component.less']
 })
 export class LoginRegisterComponent implements OnInit {
+  public form: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.getFormData()
+  }
+
+  getFormData(): void {
+    this.form = new FormGroup({
+      'nome': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+      'sobrenome': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(80)]),
+      'telefone': new FormControl(null, [Validators.required, Validators.pattern(/^[0-9]{11}$/)]),
+      'email': new FormControl(null, [Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+      'senha': new FormControl(null, [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)]),
+      'confirmarSenha': new FormControl(null, [Validators.required, matchPasswordValidator('senha')])
+    });
   }
 
 }
