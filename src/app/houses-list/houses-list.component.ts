0fc1828a4f3f6d4070a3d44b3a5aed9064676601
danillo <danillopkt@gmail.com>
2018-11-08 14:05:59 -0200

    update

--- src/app/houses-list/houses-list.component.ts
+++ src/app/houses-list/houses-list.component.ts
@@ -1,64 +1,79 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
-import { HousesService } from '../houses.service';
-import { House } from 'src/app/shared/house.model';
-import { ErrorHandlerService } from '../error-handler.service';
-import { FavoriteService } from '../favorite.service';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2
+} from "@angular/core";
+import { HousesService } from "../houses.service";
+import { House } from "src/app/shared/house.model";
+import { ErrorHandlerService } from "../error-handler.service";
+import { FavoriteService } from "../favorite.service";
+import { LogStateService } from '../log-state.service';
 
 @Component({
-  selector: 'app-houses-list',
-  templateUrl: './houses-list.component.html',
-  styleUrls: ['./houses-list.component.less']
+  selector: "app-houses-list",
+  templateUrl: "./houses-list.component.html",
+  styleUrls: ["./houses-list.component.less"]
 })
 export class HousesListComponent implements OnInit {
-  //public housesMock: House[] = HOUSES;
-  @ViewChild('icon') icon: ElementRef
+  @ViewChild("icon")
+  icon: ElementRef;
+  public isLogged: boolean;
   public conectionError: boolean;
   public houses: House[];
-  public favoriteUrl: string = '../../assets/coracao_vazio.png';
+  public favoriteUrl: string = "../../assets/coracao_vazio.png";
   constructor(
     private housesService: HousesService,
     private errorService: ErrorHandlerService,
     public renderer: Renderer2,
-    public favoriteService: FavoriteService
-    ) { }
+    public favoriteService: FavoriteService,
+    public logStateService: LogStateService
+  ) {}
 
+  ngOnInit() {
+    this.getHousesList();
+    this.checkState();
+  }
 
-
-    ngOnInit() {
-      this.getHousesList()
-    }
-
-    getHousesList(){
-      this.housesService.getHouses()
+  getHousesList() {
+    this.housesService
+      .getHouses()
       .subscribe(
-        data=> this.houses = data.imoveis,
-        error => this.errorService.error.subscribe(state => this.conectionError = state)
-        )
-      }
+        data => (this.houses = data.imoveis),
+        error =>
+          this.errorService.error.subscribe(
+            state => (this.conectionError = state)
+          )
+      );
+  }
 
-      favoriteToggle(event){
-        let target = event.srcElement
-        let id = target.attributes.id.nodeValue
-        let src = target.attributes.src.value
-        this.changeImgSrc(id,src)
-      }
+  favoriteToggle(event) {
+    let target = event.srcElement;
+    let id = target.attributes.id.nodeValue;
+    let src = target.attributes.src.value;
+    this.changeImgSrc(id, src);
+  }
 
-      changeImgSrc(id, src){
-        if(src.includes('vazio')){
-          (<HTMLInputElement>document.getElementById(id)).src = '../../assets/coracao_cheio.png';
-          this.editFavoriteList(id, true)
-        }else{
-          (<HTMLInputElement>document.getElementById(id)).src = '../../assets/coracao_vazio.png';
-          this.editFavoriteList(id, false)
-        }
-      }
-
-      editFavoriteList(id, action){
-        this.favoriteService.setFavorite(id, action)
-      }
-
-
-
+  changeImgSrc(id, src) {
+    if (src.includes("vazio")) {
+      (<HTMLInputElement>document.getElementById(id)).src =
+        "../../assets/coracao_cheio.png";
+      this.editFavoriteList(id, true);
+    } else {
+      (<HTMLInputElement>document.getElementById(id)).src =
+        "../../assets/coracao_vazio.png";
+      this.editFavoriteList(id, false);
     }
+  }
 
+  editFavoriteList(id, action) {
+    this.favoriteService.setFavorite(id, action);
+  }
 
+  checkState() {
+    this.logStateService.atualState.subscribe(state => {
+      this.isLogged = state;
+    });
+  }
+}
